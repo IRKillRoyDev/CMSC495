bfec655fa7fca08a00b54a1e6c82647bfd9937e7
Irkillroy <37928065+Irkillroy@users.noreply.github.com>
2022-05-28 11:01:03 -0700

    Added more web scraping module without making them stand alone module

--- WebScrape.py
+++ WebScrape.py
@@ -1,11 +1,56 @@
+import csv
 import requests
 from bs4 import BeautifulSoup as bs
 
-URL = 'https://www.geeksforgeeks.org/page/1/'
 
-req = requests.get(URL)
-soup = bs(req.text, 'html.parser')
+URL = 'https://www.realtor.com/soldhomes'
 
-titles = soup.find_all('div', attrs = {'class','head'})
+soup = bs("req.text", 'html.parser')
 
-print(titles[4].text)
+titles = soup.find_all('div', attrs={'class', 'head'})
+titles_list = []
+
+COUNT = 1
+for title in titles:
+    d = {}
+    d['Title Number'] = f'Title {COUNT}'
+    d['Title Name'] = title.text
+    COUNT += 1
+    titles_list.append(d)
+
+FILENAME = 'titles.csv'
+with open(FILENAME, 'w', newline='') as f:
+    w = csv.DictWriter(f,['Title Number','Title Name'])
+    w.writeheader()
+
+    w.writerows(titles_list)
+
+# Making a GET request
+r = requests.get('https://www.realtor.com/soldhomes')
+
+# Parsing the HTML
+soup = bs(r.content, 'html.parser')
+
+images_list = []
+
+images = soup.select('img')
+for image in images:
+    src = image.get('src')
+    alt = image.get('alt')
+    images_list.append({"src": src, "alt": alt})
+
+for image in images_list:
+    print(image)
+
+# Making a GET request
+r = requests.get('https://www.realtor.com/soldhomes')
+
+# Parsing the HTML
+soup = bs(r.content, 'html.parser')
+
+s = soup.find('div', class_='entry-content')
+
+lines = s.find_all('p')
+
+for line in lines:
+    print(line.text)
