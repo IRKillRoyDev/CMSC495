84eb5c81a27526af14402a533f25cb954e443ac8
Irkillroy <37928065+Irkillroy@users.noreply.github.com>
2022-06-20 20:18:24 -0700

    Created a Dunder Main.  This main function pulls the data out of the CSV and prints it in two different formats.

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import csv
 
 def main():
     with open('titles.csv', 'r') as homefile:
-        print(sum([len(homefile[x]) for x in homefile if isinstance(homefile[x], list)]))
-
+        for line in csv.DictReader(homefile): # DictReader is a reader that returns a dictionary from the source.
+            for key in line:
+                print(key + ': ' + line[key]) #Prints all the keys and values in the csv file in a certain format.
+            print(line) #Prints the Dictionary entire line.
 if __name__ == '__main__':
     main()
