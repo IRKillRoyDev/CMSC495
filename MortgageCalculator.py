45cd3e8728bf97c785e210253cbb3981db19f760
ryoung8501 <64854126+ryoung8501@users.noreply.github.com>
2022-06-29 19:25:42 -0400

    Added Error checking and validation
    Added error checking and validation so that only positive floats or integers are entered by the user, depending on the value being asked for.  A helper method was written to check whether a number is a float

--- MortgageCalculator.py
+++ MortgageCalculator.py
@@ -1,38 +1,71 @@
 from tkinter import *
 
+    #"""Revision History
+    #2 JUN - Created test mortgage calculator program to determine formulas for all calculations
+    #16 JUN - Created dummy main program to call mortgage calculator from to test functionality
+    #25 JUN - Created clean mortgage calculator program based on test program written earlier
+    #25 JUN - Changed data types for user input based on ICD Document
+    #26 JUN - Separated all calculations into different functions within program as per project analysis document
+    #27 JUN - Placed display functionality into its own function as per project analysis document
+    #"""
+
 root = Tk()
 root.title('Mortgage Calculator')
 root.geometry("500x400")
 
 
+# Checks Whether A Value Is A Float
+def isfloat(num):
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
+
+
 # Converts Loan Term From Years to Months
 def loan_term_conversion():
 	if term_entry.get():
-		termYears = int(term_entry.get())
-		termMonths = termYears * 12
-		return termMonths
+		if term_entry.get().isdigit():
+			termYears = int(term_entry.get())
+			termMonths = termYears * 12
+			return termMonths
+		else:
+			loan_amount_label.config(text="")
+			payment_label.config(text="Please Only Enter Positive Integers")
 	else:
+		loan_amount_label.config(text="")
 		payment_label.config(text="Please Don't Leave Any Boxes Blank")
 
 
 # Converts Overall Interest Rate into Monthly Interest Rate
 def monthly_interest_rate():
 	if interest_entry.get():
-		interestRate = float(interest_entry.get())
-		monthlyRate = interestRate / 100 / 12
-		return monthlyRate
+		if isfloat(interest_entry.get()):
+			interestRate = float(interest_entry.get())
+			monthlyRate = interestRate / 100 / 12
+			return monthlyRate
+		else:
+			loan_amount_label.config(text="")
+			payment_label.config(text="Please Only Enter A Decimal Value")
 	else:
+		loan_amount_label.config(text="")
 		payment_label.config(text="Please Don't Leave Any Boxes Blank")
 
 
 # Calculates Total Amount of the Loan
 def loan_amount():
 	if amount_entry.get() and down_payment_entry.get():
-		houseCost = float(amount_entry.get())
-		downPayment = float(down_payment_entry.get())
-		loanAmount = houseCost - downPayment
-		return loanAmount
+		if isfloat(amount_entry.get()) and isfloat(down_payment_entry.get()):
+			houseCost = float(amount_entry.get())
+			downPayment = float(down_payment_entry.get())
+			loanAmount = houseCost - downPayment
+			return loanAmount
+		else:
+			loan_amount_label.config(text="")
+			payment_label.config(text="Please Only Enter A Decimal Value")
 	else:
+		loan_amount_label.config(text="")
 		payment_label.config(text="Please Don't Leave Any Boxes Blank")
 
 
@@ -77,7 +110,7 @@ amount_entry = Entry(my_frame, font=("Helvetica", 18))
 interest_label = Label(my_frame, text="Interest Rate")
 interest_entry = Entry(my_frame, font=("Helvetica", 18))
 
-term_label = Label(my_frame, text="Term (years)")
+term_label = Label(my_frame, text="Term (Years)")
 term_entry = Entry(my_frame, font=("Helvetica", 18))
 
 down_payment_label = Label(my_frame, text="Down Payment Amount")
