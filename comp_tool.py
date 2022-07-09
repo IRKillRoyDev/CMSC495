96361935e2af0c7c8f47ae5c0800395e07efc0ef
Irkillroy <37928065+Irkillroy@users.noreply.github.com>
2022-07-09 16:35:38 -0700

    Yes, what my last commit said... ?

--- comp_tool.py
+++ comp_tool.py
@@ -5,23 +5,12 @@ from tkinter import ttk
 import pandas as pd
 import numpy as np
 
-"""Revision History
-    May 28 - Added function to open CSV
-    20 Jun - Created the main function that pulls the data out of the CSV
-    25 Jun - Added a window for the Comp Tool
-    25 Jun - Added a 'List' button
-    25 Jun - Added an initial value to user inputs
-    25 Jun - Added function to list the house data
-    26 Jun - Used Pandas to format the house data in the 'list' of homes
-    27 Jun - Moved the functions to the top of the __init__.
-    3 Jul - Updated the synthetic data to have approx 3000 entries of randomized numbers
-    4 Jul - finished adding the filter alogrithm to sort the data
-    """
 class HomeCompTool:
     """Class Doc String Summary Here
     """
     data = pd.read_csv('titles.csv')
     df = pd.DataFrame(data).set_index('Location')
+
     #filtered_values = df.query(sqft_str + ' & ' + baths_str + ' & ' + beds_str)
 
     def __init__(self, root):
@@ -46,6 +35,7 @@ class HomeCompTool:
             """
             data = pd.read_csv('titles.csv')
             df = pd.DataFrame(data).set_index('Location')
+            df['Price'] = df['Price'].map('${:,}'.format)
             write_to_log(df)
 
         def sort(*args):
@@ -58,19 +48,24 @@ class HomeCompTool:
                 baths = float(self.bath.get())
                 beds = float(self.bed.get())
                 #Pending call to alogrithm
-                high_sqft = sqft + 50
-                low_sqft = sqft - 50
-                sqft_str = str(low_sqft)+ ' <= Size <= '+ str(high_sqft)
-                high_baths = baths + 1
-                low_baths = baths - 1
-                baths_str = str(low_baths)+ ' <= Bath <= '+ str(high_baths)
-                high_beds = beds + 1
-                low_beds = beds - 1
-                beds_str = str(low_beds)+' <= Bedroom <= '+ str(high_beds)
-
-                filtered_values = df.query(sqft_str + ' & ' + baths_str + ' & ' + beds_str)
-                self.comps.set(filtered_values['Price'].mean())
-                write_to_log(filtered_values)
+                high_sqft = sqft + 50 #creates the high side of the range
+                low_sqft = sqft - 50 #creates the low side of the range
+                sqft_str = str(low_sqft)+ ' <= Size <= '+ str(high_sqft) #builds the query string
+                high_baths = baths + 1 #creates the high side of the range
+                low_baths = baths - 1 #creates the low side of the range
+                baths_str = str(low_baths)+ ' <= Bath <= '+ str(high_baths) #builds the query string
+                high_beds = beds + 1 #creates the high side of the range
+                low_beds = beds - 1 #creates the low side of the range
+                beds_str = str(low_beds)+' <= Bedroom <= '+ str(high_beds) #builds the query string
+                sale_str = '`Date of Sale` <= "3 months ago"' #creates the query string
+                filtered_query = df.query(sqft_str + ' & ' + baths_str + ' & ' + beds_str +
+                                           ' & ' + sale_str) #Searches using search parameters
+                #df['Price'] = df['Price'].map('${:,.2f}'.format)
+                #df['Price'] = pd.to_numeric(df['Price'],errors='coerce')
+                comp_mean = filtered_query['Price'].median()#finds the mean of the query
+                self.comps.set('${:,.0f}'.format(comp_mean)) #sets the mean
+                filtered_query['Price'] = filtered_query['Price'].map('${:,}'.format)
+                write_to_log(filtered_query) #writes the query to the window
             except ValueError:
                 pass
 
